bc2c9c53fba2d56ef7987c22240c292784b5d50c
bsundin <bensundin@proton.me>
2025-02-26 14:43:18 -0700

    Adds translation form and output textarea
    Adds a translation form with language selection, input textarea, and translate button. Also, includes an output textarea for displaying the translated text.
    
    Fixes #123

---
+++ js/formSubmit.js
@@ -0,0 +1,7 @@
+function submitTranslation() {
+    console.log("test");
+    var input = document.getElementById("input").value;
+    var output = document.getElementById("output");
+    document.getElementById("output").innerHTML = input;
+    console.log(input);
+}
