d39def9221370a5289d2238dca8be07e87212083
bsundin <bensundin@proton.me>
2025-02-28 14:44:10 -0700

    Improves form functionality and UI in translation web app
    Adds overflow and box shadow styles to the textarea for better user experience
    Updates select options in the "from" and "to" dropdowns to include "Auto" for automatic language detection
    Adds onchange event handlers to the "from" and "to" dropdowns for language selection

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -1,7 +1,23 @@
+var fromLang = "auto";
+var toLang = "en";
+
 function submitTranslation() {
     console.log("test");
     var input = document.getElementById("input").value;
     var output = document.getElementById("output");
     document.getElementById("output").innerHTML = input;
     console.log(input);
+}
+function selectFromLanguage() {
+    console.log("from changed");
+    var from = document.getElementById("from");
+    var to = document.getElementById("to");
+    if (from.value != "auto") {
+        to.add(fromLang);
+        to.remove(from.id);
+    }
+}
+
+function selectToLanguage() {
+    console.log("to changed");
 }
