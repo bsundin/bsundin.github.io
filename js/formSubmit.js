dafae12940dfb4837fab10733d231a5d45027964
bsundin <bensundin@proton.me>
2025-03-04 14:44:26 -0700

    Implements language dropdown options in translation form
    Adds dynamic loading of language options in the "from" and "to" dropdown menus in the translation form
    Updates the JavaScript code to populate the dropdown menus with language codes and corresponding names
    Improves the user experience by allowing users to select their desired languages for translation
    
    Fixes #123

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -1,24 +1,41 @@
 var fromLang = "auto";
 var toLang = "en";
-const langCodeArray = ["sq", "ar", "az", "eu", "bn", "bg", "ca", "zt", "zh", "cs", "da", "nl", "en", "eo", "et", "fi", "fr", "gl", "de"];
+const langCodeArray = ["sq", "ar", "az", "eu", "bn", "bg", "ca", "zt", "zh", "cs", "da", "nl", "en", "eo", "et", "fi", "fr", "gl", "de", "el", "he", "hi", "hu", "id", "ga", "it", "ja", "ko", "lv", "lt", "ms", "nb", "fa", "pl", "pt", "ro", "ru", "sk", "sl", "es", "sv", "tl", "th", "tr", "uk", "ur"];
+const langNameArray = ["Albanian", "Arabic", "Azerbaijani", "Basque", "Bengali", "Bulgarian", "Catalan", "Chinese (Traditional)", "Chinese (Simplified)", "Czech", "Danish", "Dutch", "English", "Esperanto", "Estonian", "Finnish", "French", "Galician", "German", "Greek", "Hebrew", "Hindi", "Hungarian", "Indonesian", "Irish", "Italian", "Japanese", "Korean", "Latvian", "Lithuanian", "Malay", "Norwegian", "Persian", "Polish", "Portuguese", "Romanian", "Russian", "Slovak", "Slovenian", "Spanish", "Swedish", "Tagalog", "Thai", "Turkish", "Ukrainian", "Urdu"];
+
+function loadDropdowns() {
+    var from = document.getElementById("from");
+    var to = document.getElementById("to");
+    for (var i = 0; i < langCodeArray.length; i++) {
+        var fromOption = document.createElement("option");
+        fromOption.value = langCodeArray[i];
+        fromOption.text = langNameArray[i];
+        from.add(fromOption);
+        var toOption = document.createElement("option");
+        toOption.value = langCodeArray[i];
+        toOption.text = langNameArray[i];
+        to.add(toOption);
+    }
+    console.log("dropdowns loaded");
+}
 
 function submitTranslation() {
     console.log("test");
     var input = document.getElementById("input").value;
     var output = document.getElementById("output");
-    document.getElementById("output").innerHTML = input;
+    output.innerHTML = input;
     console.log(input);
 }
-function selectFromLanguage() {
-    console.log("from changed");
-    var from = document.getElementById("from");
-    var to = document.getElementById("to");
-    if (from.value != "auto") {
-        to.add(fromLang);
-        to.remove(from.id);
-    }
-}
+// function selectFromLanguage() {
+//     console.log("from changed");
+//     var from = document.getElementById("from");
+//     var to = document.getElementById("to");
+//     if (from.value != "auto") {
+//         to.add(fromLang);
+//         to.remove(from.id);
+//     }
+// }
 
-function selectToLanguage() {
-    console.log("to changed");
-}
+// function selectToLanguage() {
+//     console.log("to changed");
+// }
